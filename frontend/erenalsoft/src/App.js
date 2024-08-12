3307770e383081586e925fb6391e3b9418027198
josvid.calderon <jncalderon7@misena.edu.co>
2024-08-12 16:07:55 -0500

    first commit react

---
+++ frontend/erenalsoft/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        this is eRenalSoft
+        </p>
+        <a
+          className="App-link"
+          href="https://www.renalsoft.net"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          renalsoft
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
